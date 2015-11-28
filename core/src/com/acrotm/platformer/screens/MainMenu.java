9109028bb75a9196ccd84fd775bc890ef2c95253
Tyler Marshall <tymarshall1@hotmail.com>
2015-11-27 20:14:33 -0800

    Initializing with project files.

---
+++ core/src/com/acrotm/platformer/screens/MainMenu.java
@@ -0,0 +1,18 @@
+package com.acrotm.platformer.screens;
+
+import com.badlogic.gdx.Game;
+
+/**
+ * Created by Tyler on 11/27/2015.
+ */
+public class MainMenu extends PlatformerScreen{
+    /**
+     * We Extend the PlatFormer Screen so we can use the methods
+     * that the Platformer screen has without having to implement Screen over
+     * and over again.
+     * @param game a Game object.
+     */
+    public MainMenu(Game game) {
+        super(game);
+    }
+}
